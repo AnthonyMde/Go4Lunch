e13eb8b81798df7388117937e4490dbf38feadfd
Anthony Mamode <amamode@appstud.com>
2019-01-11 01:23:21 +0100

    Create login page with google connection.

---
+++ app/src/main/java/com/my/anthonymamode/go4lunch/HomeActivity.kt
@@ -0,0 +1,33 @@
+package com.my.anthonymamode.go4lunch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        bottomNavBar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_map -> {
+                // TODO: fragment transaction (map fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_list -> {
+                // TODO: fragment transaction (list fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_workmates -> {
+                // TODO: fragment transaction (workmates fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+}
---
+++ app/src/main/java/com/my/anthonymamode/go4lunch/LoginActivity.kt
@@ -0,0 +1,65 @@
+package com.my.anthonymamode.go4lunch
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private val RC_SIGN_IN = 123
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        loginGoogleButton.setOnClickListener {
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+            )
+
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        redirectAfterSignin(requestCode, resultCode, data)
+    }
+
+    private fun redirectAfterSignin(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == RC_SIGN_IN) {
+            when (resultCode) {
+                Activity.RESULT_OK -> launchHomeActivity()
+                Activity.RESULT_CANCELED -> {
+                    val response = IdpResponse.fromResultIntent(data)
+                    when {
+                        response == null -> showCanceledSnackBar(R.string.login_canceled)
+                        response.errorCode == ErrorCodes.NO_NETWORK -> showCanceledSnackBar(R.string.login_no_network)
+                        response.errorCode == ErrorCodes.UNKNOWN_ERROR -> showCanceledSnackBar(R.string.login_unknown_error)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun launchHomeActivity() {
+        intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun showCanceledSnackBar(errorMessage: Int) {
+        Snackbar.make(loginActivityLayout, errorMessage, Snackbar.LENGTH_SHORT)
+    }
+}
