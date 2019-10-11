9e5fc11717cfd7f541a58b498d7bbb85a00cce0e
Anthony Mamode <amamode@appstud.com>
2019-10-11 17:53:16 +0200

    [FIX] Stop listening firebase RV in onPause lifecycle.

--- app/src/main/java/com/my/anthonymamode/go4lunch/ui/home/workmates/WorkmatesFragment.kt
+++ app/src/main/java/com/my/anthonymamode/go4lunch/ui/home/workmates/WorkmatesFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.my.anthonymamode.go4lunch.R
 import com.my.anthonymamode.go4lunch.data.api.getUsersOrderedByLunch
@@ -19,6 +20,7 @@ import com.my.anthonymamode.go4lunch.utils.BaseFragment
 import kotlinx.android.synthetic.main.fragment_workmates.*
 
 class WorkmatesFragment : BaseFragment() {
+    private var mAdapter: FirestoreRecyclerAdapter<User, WorkmatesAdapter.WorkmatesViewHolder>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,26 +36,44 @@ class WorkmatesFragment : BaseFragment() {
         activity?.let { configureRecyclerView(it) }
     }
 
+    override fun onPause() {
+        super.onPause()
+        mAdapter?.stopListening()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mAdapter?.startListening()
+    }
+
     /**
      * Set the adapter for the recycler and pass the data through it here
      */
     private fun configureRecyclerView(activity: FragmentActivity) {
         val viewModel = ViewModelProviders.of(activity).get(HomeViewModel::class.java)
         val userId = viewModel.userId
-        workmatesRecyclerView.adapter =
-            WorkmatesAdapter(
-                generateOptionForAdapter(),
-                userId,
-                WorkmateListType.ALL,
-                onItemClick = { placeId ->
-                    val intent = Intent(context, DetailRestaurantActivity::class.java)
-                    intent.putExtra("placeId", placeId)
-                    startActivity(intent)
-                },
-                onChatIconClick = { workmateId, workmateName ->
-                    context?.let { ChatActivity.navigateToChatActivity(workmateId, workmateName, it) }
-                })
-        workmatesRecyclerView.layoutManager = LinearLayoutManager(context)
+        mAdapter = WorkmatesAdapter(
+            generateOptionForAdapter(),
+            userId,
+            WorkmateListType.ALL,
+            onItemClick = { placeId ->
+                val intent = Intent(context, DetailRestaurantActivity::class.java)
+                intent.putExtra("placeId", placeId)
+                startActivity(intent)
+            },
+            onChatIconClick = { workmateId, workmateName ->
+                context?.let {
+                    ChatActivity.navigateToChatActivity(
+                        workmateId,
+                        workmateName,
+                        it
+                    )
+                }
+            })
+        workmatesRecyclerView.apply {
+            adapter = mAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 
     /**
