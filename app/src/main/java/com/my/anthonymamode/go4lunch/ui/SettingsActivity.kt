f65488e27882d0e11420e6f75d4a8ccc982c2067
Anthony Mamode <amamode@appstud.com>
2019-11-01 12:31:05 +0100

    [feat] Notifications settings.

--- app/src/main/java/com/my/anthonymamode/go4lunch/ui/SettingsActivity.kt
+++ app/src/main/java/com/my/anthonymamode/go4lunch/ui/SettingsActivity.kt
@@ -3,14 +3,20 @@ package com.my.anthonymamode.go4lunch.ui
 import android.content.Context
 import android.content.DialogInterface.BUTTON_NEGATIVE
 import android.content.DialogInterface.BUTTON_POSITIVE
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.res.ResourcesCompat
 import com.firebase.ui.auth.AuthUI
 import com.google.android.gms.tasks.OnSuccessListener
 import com.my.anthonymamode.go4lunch.R
 import com.my.anthonymamode.go4lunch.data.api.deleteUser
 import com.my.anthonymamode.go4lunch.utils.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_settings.deleteAccountButton
+import kotlinx.android.synthetic.main.activity_settings.settingsNotificationSwitch
 import kotlinx.android.synthetic.main.activity_settings.toolbar
 import org.jetbrains.anko.startActivity
 
@@ -25,6 +31,15 @@ class SettingsActivity : BaseActivity() {
         setContentView(R.layout.activity_settings)
         configureToolbar()
         setListeners()
+
+        settingsNotificationSwitch.isChecked =
+            NotificationManagerCompat.from(this).areNotificationsEnabled()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        settingsNotificationSwitch.isChecked =
+            NotificationManagerCompat.from(this).areNotificationsEnabled()
     }
 
     private fun setListeners() {
@@ -36,6 +51,12 @@ class SettingsActivity : BaseActivity() {
                 showToastError(getString(R.string.settings_delete_account_error))
                 redirectToLogin()
             }
+        }
+
+        settingsNotificationSwitch.setOnClickListener {
+            settingsNotificationSwitch.isChecked = !settingsNotificationSwitch.isChecked
+            val intent = createNotificationSettingsIntent(this)
+            startActivity(intent)
         }
     }
 
@@ -61,10 +82,18 @@ class SettingsActivity : BaseActivity() {
                 setButton(
                     BUTTON_NEGATIVE,
                     getString(R.string.settings_delete_account_alert_dialog_negative_button)
-                ) { dialog, _ -> dialog.dismiss() }
-                this.show()
-                getButton(BUTTON_POSITIVE)?.setTextColor(resources.getColor(android.R.color.holo_red_light))
-                this.show()
+                ) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                getButton(BUTTON_POSITIVE)?.setTextColor(
+                    ResourcesCompat.getColor(
+                        resources,
+                        android.R.color.holo_red_light,
+                        null
+                    )
+                )
+
+                show()
             }
     }
 
@@ -93,5 +122,22 @@ class SettingsActivity : BaseActivity() {
     private fun configureToolbar() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun createNotificationSettingsIntent(context: Context?): Intent {
+        val intent = Intent()
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
+                intent.action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+                intent.putExtra(Settings.EXTRA_APP_PACKAGE, context?.packageName)
+            }
+            else -> {
+                intent.action = "android.settings.APP_NOTIFICATION_SETTINGS"
+                intent.putExtra("app_package", context?.packageName)
+                intent.putExtra("app_uid", context?.applicationInfo?.uid)
+            }
+        }
+
+        return intent
     }
 }
